148c475475a4c8e7311c14b2254b22f119920593
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-27 16:34:57 +0430

    Preprocessing the brain

--- brain.py
+++ brain.py
@@ -1,4 +1,5 @@
-import random 
+import numpy as np 
+import random
 
 human_data = "human.txt"
 robot_data = "robot.txt"
@@ -10,3 +11,7 @@ with open(robot_data) as chats_2:
     robot_chats = chats_2.read().split('\n')
 
 print(f'Human chats: {len(human_chats)}, Robot chats: {len(robot_chats)}')
+
+pairs = list(zip(human_chats, robot_chats))
+
+
