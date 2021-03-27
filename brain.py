f0b69671cfe2d0c75b8cff0b60b5b1790b5e0fae
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-28 04:12:49 +0430

    Now it can generate her own mind

--- brain.py
+++ brain.py
@@ -88,3 +88,22 @@ for line, (input_doc, target_doc) in enumerate(zip(input_docs, target_docs)):
             decoder_target_data[line, timestep - 1, target_features_dict[token]] = 1.
 
 
+dimensionality = 256 
+batch_size = 10 
+epochs = 15000 
+
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder_lstm = LSTM(dimensionality, return_state=True)
+encoder_outputs, state_hidden, state_cell = encoder_lstm(encoder_inputs) 
+encoder_states = [state_hidden, state_cell]
+
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+decoder_lstm = LSTM(dimensionality, return_sequences=True, return_state=True)
+decoder_outputs, decoder_state_hidden, decoder_state_cell = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+deocder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = deocder_dense(decoder_outputs)
+
+training_model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+training_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'], sample_weight_mode = 'temporal')
+training_model.fit([encoder_input_data, decoder_input_data], decoder_target_data, batch_size = batch_size, epochs = epochs, validation_split=0.2)
+training_model.save('mind.h5')
