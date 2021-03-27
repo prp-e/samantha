7b7a8091cd064a95a01c4f657810459a217764c3
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-03-27 16:31:53 +0430

    working on Samantha's mind

---
+++ brain.py
@@ -0,0 +1,12 @@
+import random 
+
+human_data = "human.txt"
+robot_data = "robot.txt"
+
+with open(human_data) as chats_1:
+    human_chats = chats_1.read().split('\n')
+
+with open(robot_data) as chats_2:
+    robot_chats = chats_2.read().split('\n')
+
+print(f'Human chats: {len(human_chats)}, Robot chats: {len(robot_chats)}')
