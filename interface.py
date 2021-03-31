7f469b1b61687217915aef90c5ec34bbb3ceb158
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-04-01 01:44:50 +0430

    Getting ready for the test

--- interface.py
+++ interface.py
@@ -2,6 +2,8 @@ from tensorflow import keras
 from keras.layers import Input, LSTM, Dense
 from keras.models import Model, load_model
 
+from brain import decoder_dense, decoder_lstm, state_cell, state_hidden, decoder_inputs
+
 training_model = load_model('mind.h5')
 
 encoder_inputs = training_model.input[0]
@@ -13,3 +15,10 @@ latent_dim = 256
 decoder_state_input_hidden = Input(shape=(latent_dim,))
 decoder_state_input_cell = Input(shape=(latent_dim,))
 decoder_states_inputs = [decoder_state_input_hidden, decoder_state_input_cell]
+
+decoder_outputs, state_hidden, state_cell = 
+decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+decoder_states = [state_hidden, state_cell]
+decoder_outputs = decoder_dense(decoder_outputs)
+
+decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
